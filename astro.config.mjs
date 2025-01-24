d34755146013e36df01501d65a3b20f8c0f3d49f
Dorell James <galangdj@gmail.com>
2025-01-25 03:15:59 +0800

    chore: remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
-
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ai.jscebu.org",
-  integrations: [
-    tailwind(),
-    sitemap({
-      changefreq: "weekly",
-      priority: 0.9,
-    }),
-  ],
+  integrations: [tailwind()],
   output: "server",
   adapter: cloudflare(),
 });
