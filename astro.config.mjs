61c0562ce36e40d2d7a5d5fc5999dc5341fcfca4
Dorell James <galangdj@gmail.com>
2025-01-25 03:11:33 +0800

    chore: SEO updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,18 @@ import tailwind from "@astrojs/tailwind";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://ai.jscebu.org",
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.9,
+    }),
+  ],
   output: "server",
   adapter: cloudflare(),
 });
